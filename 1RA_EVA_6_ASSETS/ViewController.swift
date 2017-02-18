bfabc83b4250da7a77db0c16547f99355c0ca655
Javier_Luna <javy_luna_jr@hotmail.com>
2017-02-17 20:54:33 -0700

    1ER_EVA_6_ASSETS

--- 1RA_EVA_6_ASSETS/ViewController.swift
+++ 1RA_EVA_6_ASSETS/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  Main.storyboard
 //  1RA_EVA_6_ASSETS
 //
 //  Created by administrador on 08/02/17.
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTexto: UILabel!
+    @IBAction func sliderValor(sender: UISlider) {
+        lblTexto.text = "\(sender.value)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
